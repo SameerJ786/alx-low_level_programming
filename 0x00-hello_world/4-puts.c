36818bc63bc0b697c993388ba172b5bc3087210c
SameerJ786 <jogeshur.sameer@gmail.com>
2023-06-15 07:12:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line with the puts function
+ * return: 0 (is success)
+ */
+int main (void){
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
