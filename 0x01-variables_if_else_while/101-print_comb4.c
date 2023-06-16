a3be88d76a95f5ec6fca2f67645e3b709ad8932a
SameerJ786 <jogeshur.sameer@gmail.com>
2023-06-16 11:55:10 -0700

    101-print_comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int first;
+	int second;
+	int third;
+
+	for (first = 0; first < 8; first++)
+	{
+		for (second = first +1; second < 9; second++)
+		{
+			for(third = second + 1; third < 10; third++)
+			{
+			putchar(first + '0');
+			putchar(second + '0');
+			putchar(third + '0');
+			if (first < 7 || second < 8 || third < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
