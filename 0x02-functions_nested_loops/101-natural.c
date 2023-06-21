e5cea64827009635e6d1f232f404d5c7e78f5320
SameerJ786 <jogeshur.sameer@gmail.com>
2023-06-21 14:51:12 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,11 +15,11 @@ for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0)
 {
-sum3 = sum3 + 1;
+sum3 = sum3 + i;
 }
 else if ((i % 5) == 0)
 {
-sum5 = sum5 + 1;
+sum5 = sum5 + i;
 }
 }
 sum = sum3 + sum5;
