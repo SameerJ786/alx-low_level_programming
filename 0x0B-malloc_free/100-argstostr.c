f7ec05b773bf5a5de71eae47ed3cb1fe364f82fc
SameerJ786 <jogeshur.sameer@gmail.com>
2023-07-11 14:43:35 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,76 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
  *
- * Return: number of words
+ * Return: a pointer to a new string, or NULL if it fails
  */
-int count_word(char *s)
+char *argstostr(int ac, char **av)
 {
-	int flag, c, w;
+	char *str, *s;
+	int i, j, k, len = 0;
 
-	flag = 0;
-	w = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (i = 0; i < ac; i++)
 	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
-	}
+		s = av[i];
+		j = 0;
 
-	return (w);
-}
-/**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
-char **strtow(char *str)
-{
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-
-	while (*(str + len))
+		while (s[j++])
+			len++;
 		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
-
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
-	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
 	}
 
-	matrix[k] = NULL;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
 
-	return (matrix);
+	for (i = 0, j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n';
+	}
+	str[j] = '\0';
+
+	return (str);
 }
